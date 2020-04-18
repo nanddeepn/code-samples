744ea58002945ea5f4255a685ee9cd5ea94fa63c
Nanddeep Nachan <NanddeepNachan@gmail.com>
2020-04-18 13:23:58 +0000

    Removed TreeView

--- SPFx/TreeView/src/webparts/treeView/loc/en-us.js
+++
@@ -1,9 +0,0 @@
-define([], function() {
-  return {
-    "PropertyPaneDescription": "Description",
-    "BasicGroupName": "Group Name",
-    "DescriptionFieldLabel": "Description Field",
-    "TreeExpandTitle": "Expand this item",
-    "TreeCollapseTitle": "Collapse this item"
-  }
-});

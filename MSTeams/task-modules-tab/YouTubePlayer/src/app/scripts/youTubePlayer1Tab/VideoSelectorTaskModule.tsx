09cf9879ee4900bbdd1c2e6cdead52c409b82db5
Nanddeep Nachan <nanddeepnachan@gmail.com>
2020-06-06 07:11:47 +0000

    Added MS Teams- Task Modules Tab example

---
+++ MSTeams/task-modules-tab/YouTubePlayer/src/app/scripts/youTubePlayer1Tab/VideoSelectorTaskModule.tsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+import { Provider, Flex, Text, Button, Header, ThemePrepared, themes, Input } from "@fluentui/react-northstar";
+import TeamsBaseComponent, { ITeamsBaseComponentState } from "msteams-react-base-component";
+import * as microsoftTeams from "@microsoft/teams-js";
+
+export interface IVideoSelectorTaskModuleState extends ITeamsBaseComponentState {
+    teamsTheme: ThemePrepared;
+    youTubeVideoId?: string;
+}
+
+export interface IVideoSelectorTaskModuleProps {
+}
+
+export class VideoSelectorTaskModule extends TeamsBaseComponent<IVideoSelectorTaskModuleProps, IVideoSelectorTaskModuleState> {
+    public componentWillMount(): void {
+        this.updateComponentTheme(this.getQueryVariable("theme"));
+        this.setState(Object.assign({}, this.state, {
+            youTubeVideoId: this.getQueryVariable("vid")
+        }));
+
+        if (this.inTeams()) {
+            microsoftTeams.initialize();
+            microsoftTeams.registerOnThemeChangeHandler(this.updateComponentTheme);
+        }
+    }
+
+    public render() {
+        return (
+            <Provider theme={this.state.teamsTheme}>
+                <Flex column gap="gap.smaller">
+                    <Text size="medium">
+                        Enter the ID of a YouTube video to show in the task module player.
+                        </Text>
+                    <Input value={this.state.youTubeVideoId} onChange={this.handleOnChanged}></Input>
+                    <Button content="Update" primary onClick={this.handleOnClick}></Button>
+                </Flex>
+            </Provider>
+        );
+    }
+
+    private handleOnChanged = (event): void => {
+        this.setState(Object.assign({}, this.state, {
+            youTubeVideoId: event.target.value
+        }));
+    }
+
+    private handleOnClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
+        microsoftTeams.tasks.submitTask(this.state.youTubeVideoId, undefined);
+    }
+
+    private updateComponentTheme = (teamsTheme: string = "default"): void => {
+        let theme: ThemePrepared;
+
+        switch (teamsTheme) {
+            case "default":
+                theme = themes.teams;
+                break;
+            case "dark":
+                theme = themes.teamsDark;
+                break;
+            case "contrast":
+                theme = themes.teamsHighContrast;
+                break;
+            default:
+                theme = themes.teams;
+                break;
+        }
+        // update the state
+        this.setState(Object.assign({}, this.state, {
+            teamsTheme: theme
+        }));
+    }
+}
---
+++ MSTeams/task-modules-tab/YouTubePlayer/src/app/scripts/youTubePlayer1Tab/YouTubePlayer1Tab.tsx
@@ -0,0 +1,173 @@
+import * as React from "react";
+// import { Provider, Flex, Text, Button, Header } from "@fluentui/react-northstar";
+import {
+    Provider,
+    Flex,
+    Text,
+    Button,
+    Header,
+    ThemePrepared,
+    themes,
+    Input
+} from "@fluentui/react-northstar";
+import TeamsBaseComponent, { ITeamsBaseComponentState } from "msteams-react-base-component";
+import * as microsoftTeams from "@microsoft/teams-js";
+
+/**
+ * State for the youTubePlayer1TabTab React component
+ */
+export interface IYouTubePlayer1TabState extends ITeamsBaseComponentState {
+    entityId?: string;
+
+    // Added by Nanddeep
+    teamsTheme: ThemePrepared;
+    youTubeVideoId?: string;
+}
+
+/**
+ * Properties for the youTubePlayer1TabTab React component
+ */
+export interface IYouTubePlayer1TabProps {
+
+}
+
+/**
+ * Implementation of the YouTube Player 1 content page
+ */
+export class YouTubePlayer1Tab extends TeamsBaseComponent<IYouTubePlayer1TabProps, IYouTubePlayer1TabState> {
+
+    public async componentWillMount() {
+        // this.updateTheme(this.getQueryVariable("theme"));
+        // Added by Nanddeep
+        this.updateComponentTheme(this.getQueryVariable("theme"));
+        this.setState(Object.assign({}, this.state, {
+            youTubeVideoId: "VlEH4vtaxp4"
+        }));
+
+        if (await this.inTeams()) {
+            microsoftTeams.initialize();
+            // microsoftTeams.registerOnThemeChangeHandler(this.updateTheme);
+            // Added by Nanddeep
+            microsoftTeams.registerOnThemeChangeHandler(this.updateComponentTheme);
+            microsoftTeams.getContext((context) => {
+                microsoftTeams.appInitialization.notifySuccess();
+                this.setState({
+                    entityId: context.entityId
+                });
+                this.updateTheme(context.theme);
+            });
+        } else {
+            this.setState({
+                entityId: "This is not hosted in Microsoft Teams"
+            });
+        }
+    }
+
+    /**
+     * The render() method to create the UI of the tab
+     */
+    // public render() {
+    //     return (
+    //         <Provider theme={this.state.theme}>
+    //             <Flex fill={true} column styles={{
+    //                 padding: ".8rem 0 .8rem .5rem"
+    //             }}>
+    //                 <Flex.Item>
+    //                     <Header content="This is your tab" />
+    //                 </Flex.Item>
+    //                 <Flex.Item>
+    //                     <div>
+
+    //                         <div>
+    //                             <Text content={this.state.entityId} />
+    //                         </div>
+
+    //                         <div>
+    //                             <Button onClick={() => alert("It worked!")}>A sample button</Button>
+    //                         </div>
+    //                     </div>
+    //                 </Flex.Item>
+    //                 <Flex.Item styles={{
+    //                     padding: ".8rem 0 .8rem .5rem"
+    //                 }}>
+    //                     <Text size="smaller" content="(C) Copyright Contoso" />
+    //                 </Flex.Item>
+    //             </Flex>
+    //         </Provider>
+    //     );
+    // }
+
+    public render() {
+        return (
+            <Provider theme={this.state.teamsTheme}>
+                <Flex column gap="gap.smaller">
+                    <Header>Task Module Demo</Header>
+                    <Text>YouTube Video ID:</Text>
+                    <Input value={this.state.youTubeVideoId} disabled></Input>
+                    <Button content="Change Video ID" onClick={this.onChangeVideo}></Button>
+                    <Button content="Show Video" primary onClick={this.onShowVideo}></Button>
+                    <Text content="(C) Copyright Contoso" size="smallest"></Text>
+                </Flex>
+            </Provider>
+        );
+    }
+
+    private appRoot(): string {
+        if (typeof window === "undefined") {
+            return "https://{{HOSTNAME}}";
+        } else {
+            return window.location.protocol + "//" + window.location.host;
+        }
+    }
+
+    private onShowVideo = (event: React.MouseEvent<HTMLButtonElement>): void => {
+        const taskModuleInfo = {
+            title: "YouTube Player",
+            url: this.appRoot() + `/youTubePlayer1Tab/player.html?vid=${this.state.youTubeVideoId}`,
+            width: 1000,
+            height: 700
+        };
+        microsoftTeams.tasks.startTask(taskModuleInfo);
+    }
+
+    private onChangeVideo = (event: React.MouseEvent<HTMLButtonElement>): void => {
+        const taskModuleInfo = {
+            title: "YouTube Video Selector",
+            url: this.appRoot() + `/youTubePlayer1Tab/selector.html?theme={theme}&vid=${this.state.youTubeVideoId}`,
+            width: 350,
+            height: 150
+        };
+
+        const submitHandler = (err: string, result: string): void => {
+            this.setState(Object.assign({}, this.state, {
+                youTubeVideoId: result
+            }));
+        };
+
+        microsoftTeams.tasks.startTask(taskModuleInfo, submitHandler);
+    }
+
+    private updateComponentTheme = (teamsTheme: string = "default"): void => {
+        let theme: ThemePrepared;
+
+        switch (teamsTheme) {
+            case "default":
+                theme = themes.teams;
+                break;
+            case "dark":
+                theme = themes.teamsDark;
+                break;
+            case "contrast":
+                theme = themes.teamsHighContrast;
+                break;
+            default:
+                theme = themes.teams;
+                break;
+        }
+
+        // update the state
+        this.setState(Object.assign({}, this.state, {
+            teamsTheme: theme
+        }));
+    }
+}

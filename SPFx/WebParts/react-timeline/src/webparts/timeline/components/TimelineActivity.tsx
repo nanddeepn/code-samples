fa5c7b54505550dbc7b21d3485f0927c86722e98
Ravi Kulkarni <ravi16a87@gmail.com>
2020-06-29 09:06:17 +0000

    Added More Menu

--- SPFx/WebParts/react-timeline/src/webparts/timeline/components/TimelineActivity.tsx
+++ SPFx/WebParts/react-timeline/src/webparts/timeline/components/TimelineActivity.tsx
@@ -2,22 +2,29 @@ import * as React from 'react';
 import styles from './Timeline.module.scss';
 import { escape } from '@microsoft/sp-lodash-subset';
 import { ITimelineActivity } from "../../../models";
-import { Card, ICardTokens, ICardSectionStyles, ICardSectionTokens } from '@uifabric/react-cards';
+import { Card,  ICardTokens, ICardSectionStyles, ICardSectionTokens } from '@uifabric/react-cards';
 import { FontWeights } from '@uifabric/styling';
 import { Icon, IIconStyles, Image, Stack, IStackTokens, Text, ITextStyles } from 'office-ui-fabric-react';
-
+import {ContextualMenuItemType} from 'office-ui-fabric-react/lib/ContextualMenu';
+import { Layer, IconButton, IButtonProps } from 'office-ui-fabric-react';
 export interface IActivityProps {
     activity: ITimelineActivity;
 }
 
-export default class TimelineActivity extends React.Component<IActivityProps, {}> {
+
+
+export default class TimelineActivity extends React.Component<IActivityProps,{}> {
+     
+   
     public render(): React.ReactElement<IActivityProps> {
+       
         const siteTextStyles: ITextStyles = {
-            root: {
-                color: '#025F52',
-                fontWeight: FontWeights.semibold,
-            },
-        };
+        root: {
+            color: '#025F52',
+            fontWeight: FontWeights.semibold,
+        },
+    };
+        
         const descriptionTextStyles: ITextStyles = {
             root: {
                 color: '#333333',
@@ -49,12 +56,40 @@ export default class TimelineActivity extends React.Component<IActivityProps, {}
         const footerCardSectionTokens: ICardSectionTokens = { padding: '0px 0px 0px 12px' };
 
         const { activity } = this.props;
-
+        
         return (
             <Stack tokens={sectionStackTokens}>
                 <p></p>
 
-                <Card aria-label="Clickable horizontal card " horizontal onClick={this.alertClicked} tokens={cardTokens}>
+                <Card aria-label="Clickable horizontal card " horizontal 
+                  
+                   tokens={cardTokens}>
+                    <IconButton id='ContextualMenuButton1'                      
+                        text=''
+                        width='30'
+                        split={false}
+                        iconProps={{ iconName: 'MoreVertical' }}
+                        style={{float:"right"}}
+                        menuIconProps={{ iconName: '' }}
+                        menuProps={{
+                            shouldFocusOnMount: true,
+                            items: [
+                                {
+                                    key: 'Edit',
+                                    name: 'Edit',
+                                    onClick: () => console.log('Edit Item ID'+activity.id)
+                                },
+                                {
+                                    key: 'divider_1',
+                                    itemType: ContextualMenuItemType.Divider
+                                },
+                                {
+                                    key: 'Delete',
+                                    name: 'Delete',
+                                    onClick: () => console.log('Delete Item ID' + activity.id) 
+                                 }                             
+                            ]
+                        }} />
                     <Card.Item fill>
                         <Image src="https://placehold.it/180x135" alt="Placeholder image." />
                     </Card.Item>
@@ -71,20 +106,18 @@ export default class TimelineActivity extends React.Component<IActivityProps, {}
                             {activity.acivityDate}
                         </Text>
                     </Card.Section>
-                    <Card.Section styles={footerCardSectionStyles} tokens={footerCardSectionTokens}>
-                        {/* <Icon iconName="RedEye" styles={iconStyles} />
-                        <Icon iconName="SingleBookmark" styles={iconStyles} /> */}
-                        {/* <Stack.Item grow={1}>
-                            <span />
-                        </Stack.Item> */}
+                    <Card.Section styles={footerCardSectionStyles} tokens={footerCardSectionTokens}>                       
                         <Icon iconName="MoreVertical" styles={iconStyles} />
                     </Card.Section>
                 </Card>
+               
             </Stack>
         );
     }
 
-    private alertClicked = (): void => {
-        alert('Clicked');
-    };
+   
+
+  
+
+   
 }

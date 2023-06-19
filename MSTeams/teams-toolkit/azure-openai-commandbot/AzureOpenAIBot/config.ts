9c841ec29aaeb0b652e08081f34791e61a00113c
Nanddeep Nachan <nanddeepnachan@gmail.com>
2023-06-19 04:42:42 +0000

    code changes

--- MSTeams/teams-toolkit/azure-openai-commandbot/AzureOpenAIBot/config.ts
+++ MSTeams/teams-toolkit/azure-openai-commandbot/AzureOpenAIBot/config.ts
@@ -1,8 +1,8 @@
 const config = {
   botId: process.env.BOT_ID,
   botPassword: process.env.BOT_PASSWORD,
-  endpoint: 'https://az-nachan-openai.openai.azure.com/',
-  azureApiKey: 'e161498fd0544299a86ef8af6b339f10'
+  endpoint: 'https://XXXX-openai.openai.azure.com/',
+  azureApiKey: 'XXXX'
 };
 
 export default config;

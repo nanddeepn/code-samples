6d5735092e0af04963b525463d786b110eb4da44
Nanddeep Nachan <nanddeepnachan@gmail.com>
2024-06-16 10:50:13 +0000

    Added az openai function calling sample

---
+++ AI/az-openai-function-calling-cs/GetStockPriceFunction.cs
@@ -0,0 +1,72 @@
+﻿using Azure.AI.OpenAI;
+using System.Text.Json;
+
+namespace az_openai_function_calling
+{
+    /// <summary>
+    /// Function to get stock price
+    /// </summary>
+    public class GetStockPriceFunction
+    {
+        static public string Name = "get_stock_price";
+
+        /// <summary>
+        /// Function definition
+        /// </summary>
+        static public FunctionDefinition GetFunctionDefinition()
+        {
+            return new FunctionDefinition()
+            {
+                Name = Name,
+                Description = "Get the stock price of company",
+                Parameters = BinaryData.FromObjectAsJson(
+                new
+                {
+                    Type = "object",
+                    Properties = new
+                    {
+                        Company = new
+                        {
+                            Type = "string",
+                            Description = "The company, e.g. Contoso",
+                        }
+                    },
+                    Required = new[] { "company" },
+                },
+                new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }),
+            };
+        }
+
+        /// <summary>
+        /// Function implementation
+        /// </summary>
+        /// <param name="location"></param>
+        /// <param name="unit"></param>
+        /// <returns></returns>
+        static public StockPrice GetStockPrice(string company)
+        {
+            Random r = new Random();
+            int range = 100;
+            double randomStockPrice = r.NextDouble() * range;
+
+            return new StockPrice() { Price = randomStockPrice, Currency = "USD" };
+        }
+    }
+
+    /// <summary>
+    /// Function argument
+    /// </summary>
+    public class StockPriceInput
+    {
+        public string Company { get; set; } = string.Empty;
+    }
+
+    /// <summary>
+    /// Return type
+    /// </summary>
+    public class StockPrice
+    {
+        public double Price { get; set; }
+        public string Currency { get; set; } = "USD";
+    }
+}
---
+++ AI/az-openai-function-calling-cs/Program.cs
@@ -0,0 +1,69 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using System.Text.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace az_openai_function_calling
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Uri openAIUri = new("https://contoso.openai.azure.com/");
+            string openAIApiKey = "";   // Add your Azure OpenAI API key
+            string model = "gpt-35-turbo-16k";
+
+            // Create Azure OpenAI client
+            OpenAIClient client = new(openAIUri, new AzureKeyCredential(openAIApiKey));
+            ChatCompletionsOptions chatCompletionsOptions = new ChatCompletionsOptions();
+
+            // Read user input
+            Console.WriteLine("System >  I can help you find stock price of a given company");
+            Console.Write("User > ");
+            string question = Console.ReadLine()?.Trim();
+            chatCompletionsOptions.Messages.Add(new(ChatRole.User, question));
+
+            // Make LLM function aware
+            FunctionDefinition getStockPriceFunctionDefinition = GetStockPriceFunction.GetFunctionDefinition();
+            chatCompletionsOptions.Functions.Add(getStockPriceFunctionDefinition);
+
+            // Call the Completion in a loop to determine if the finish reason is "function" or "stop."
+            ChatCompletions response = await client.GetChatCompletionsAsync(model, chatCompletionsOptions);
+            ChatChoice responseChoice = response.Choices[0];
+
+            // Loop until the finish reason is not "function."
+            while (responseChoice.FinishReason == CompletionsFinishReason.FunctionCall)
+            {
+                // Add message to history.
+                chatCompletionsOptions.Messages.Add(responseChoice.Message);
+
+                if (responseChoice.Message.FunctionCall.Name == GetStockPriceFunction.Name)
+                {
+                    string unvalidatedArguments = responseChoice.Message.FunctionCall.Arguments;
+                    StockPriceInput input = JsonSerializer.Deserialize<StockPriceInput>(unvalidatedArguments, new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase })!;
+
+                    var functionResultData = GetStockPriceFunction.GetStockPrice(input.Company);
+                    var functionResponseMessage = new ChatMessage(
+                        ChatRole.Function,
+                        JsonSerializer.Serialize(
+                            functionResultData,
+                            new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }
+                    ));
+
+                    functionResponseMessage.Name = GetStockPriceFunction.Name;
+                    chatCompletionsOptions.Messages.Add(functionResponseMessage);
+                }
+
+                // Call LLM again to generate the response.
+                response = await client.GetChatCompletionsAsync(model, chatCompletionsOptions);
+                responseChoice = response.Choices[0];
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(responseChoice.Message.Content);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Finished");
+        }
+    }
+}
